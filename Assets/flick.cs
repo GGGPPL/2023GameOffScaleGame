4ef3fa62c54c8cb0c81372e7d675ed0f6a406871
KoalaWill <108674407+KoalaWill@users.noreply.github.com>
2023-12-02 02:47:54 +0800

    add stage3 to build

---
+++ Assets/flick.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flick : MonoBehaviour
+{
+    public bool activate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activate = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
